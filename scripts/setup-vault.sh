a3f5a5920a636334826c34eb27b556eb00b32a44
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-09-21 14:53:51 -0700

    Update setup-vault.sh

--- scripts/setup-vault.sh
+++ scripts/setup-vault.sh
@@ -13,10 +13,13 @@ set -euo pipefail
 export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID:-"buildkite-is-cool"}"
 export VAULT_ADDRESS="http://127.0.0.1:8220"
 
+# enable secret mountpath kv
+vault secrets enable kv
 
 # put some data in the kv store
 
 vault kv put secret/buildkite/vault-buildkite-demo/env some_value="alpacas" && sleep 1
+vault kv put kv/buildkite/vault-buildkite-demo/env artifactory_user="test" && sleep 1
 
 # enable approle authentication
 vault auth enable approle && sleep 1
